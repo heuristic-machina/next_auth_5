35d5792ab6932700a49cd6dcd22fabf3d3ee107d
heuristic_machina <machine_learner@yahoo.com>
2024-02-20 03:39:12 -0700

    login wired

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
-// import Image from "next/image";
-// import Link from 'next/link';
-
+import { Poppins } from 'next/font/google';
 import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { LoginButton } from '@/components/auth/login-button';
+
+
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['600']
+})
 
 export default function Home() {
   return (
-      <Button size='lg'>Click Me!</Button>
+    <main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-cyan-800 to-slate-800'>
+      <div className="space-y-6 text-center">
+        <h1 className={cn(
+          "text-6xl font-semibold text-zinc drop-shadow-md",
+          font.className
+          )}>
+          Auth 🔒
+        </h1>
+        <p className='text-amber-600 text-lg'>
+          A simple authentication service
+        </p>
+        <LoginButton>
+          <Button size='lg'>
+            Sign in
+          </Button>
+        </LoginButton>
+      </div>
+    </main>
   );
 }
