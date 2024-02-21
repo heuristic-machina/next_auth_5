d8da0b80d12a36e85f324175dc6267310b1baaf9
heuristic_machina <machine_learner@yahoo.com>
2024-02-21 01:28:49 -0700

    auth fields

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -57,6 +57,23 @@ export const LoginForm = () => {
                             </FormItem>
                         )}
                         />
+                        <FormField
+                        control={form.control}
+                        name='password'
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input
+                                    {...field}
+                                    placeholder='●●●●●●●●'
+                                    type='password'
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                        />
                     </div>
                 </form>
             </Form>
--- schemas/index.ts
+++ schemas/index.ts
@@ -2,7 +2,9 @@ import * as z from 'zod';
 
 
 export const LoginSchema = z.object({
-    email: z.string().email(),
+    email: z.string().email({
+        message: 'Email is required'
+    }),
     password: z.string()
 });
 
